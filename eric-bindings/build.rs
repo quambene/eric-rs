931d93c215d1b63224aa58d5a61fba1350c33b6e
quambene <contact.quambene@gmail.com>
2024-08-03 03:03:37 +0200

    Fix build script

--- eric-bindings/build.rs
+++ eric-bindings/build.rs
@@ -1,38 +1,43 @@
-use std::{
-    env, io,
-    path::{Path, PathBuf},
-};
+use std::io;
 
-#[cfg(not(feature = "docs-rs"))]
 fn main() -> io::Result<()> {
-    let library_path =
-        env::var("LIBRARY_PATH").expect("Missing environment variable 'LIBRARY_PATH'");
-    let library_name =
-        env::var("LIBRARY_NAME").expect("Missing environment variable 'LIBRARY_NAME'");
-    let header_file = env::var("HEADER_FILE").expect("Missing environment variable 'HEADER_FILE'");
+    #[cfg(not(feature = "docs-rs"))]
+    {
+        use std::{
+            env,
+            path::{Path, PathBuf},
+        };
 
-    let library_path = Path::new(&library_path);
-    let header_file = Path::new(&header_file);
+        let library_path =
+            env::var("LIBRARY_PATH").expect("Missing environment variable 'LIBRARY_PATH'");
+        let library_name =
+            env::var("LIBRARY_NAME").expect("Missing environment variable 'LIBRARY_NAME'");
+        let header_file =
+            env::var("HEADER_FILE").expect("Missing environment variable 'HEADER_FILE'");
 
-    println!("cargo:rustc-link-search={}", library_path.display());
-    println!("cargo:rustc-link-lib={}", library_name);
-    println!("cargo:rerun-if-changed={}", header_file.display());
-    println!("cargo:rustc-env=LD_LIBRARY_PATH={}", library_path.display());
+        let library_path = Path::new(&library_path);
+        let header_file = Path::new(&header_file);
 
-    let header = header_file.to_str().expect("Can't convert path to string");
+        println!("cargo:rustc-link-search={}", library_path.display());
+        println!("cargo:rustc-link-lib={}", library_name);
+        println!("cargo:rerun-if-changed={}", header_file.display());
+        println!("cargo:rustc-env=LD_LIBRARY_PATH={}", library_path.display());
 
-    let bindings = bindgen::Builder::default()
-        .header(header)
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        .generate()
-        .expect("Can't generate bindings");
+        let header = header_file.to_str().expect("Can't convert path to string");
 
-    let out_dir = env::var("OUT_DIR").expect("Can't read environment variable 'OUT_DIR'");
-    let output_path = PathBuf::from(out_dir);
+        let bindings = bindgen::Builder::default()
+            .header(header)
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+            .generate()
+            .expect("Can't generate bindings");
 
-    bindings
-        .write_to_file(output_path.join("bindings.rs"))
-        .expect("Can't write bindings");
+        let out_dir = env::var("OUT_DIR").expect("Can't read environment variable 'OUT_DIR'");
+        let output_path = PathBuf::from(out_dir);
+
+        bindings
+            .write_to_file(output_path.join("bindings.rs"))
+            .expect("Can't write bindings");
+    }
 
     Ok(())
 }
