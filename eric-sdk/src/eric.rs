86467f6536c1bb295148f9a21aeecaedb3c68f21
quambene <contact.quambene@gmail.com>
2024-08-04 04:15:20 +0200

    Validate and send xml

---
+++ eric-sdk/src/eric.rs
@@ -0,0 +1,209 @@
+use crate::{
+    config::{CertificateConfig, PrintConfig},
+    error_code::ErrorCode,
+    response::{EricResponse, ResponseBuffer},
+    utils::ToCString,
+    ProcessingFlag,
+};
+use anyhow::{anyhow, Context};
+use eric_bindings::{EricBearbeiteVorgang, EricBeende, EricDekodiereDaten, EricInitialisiere};
+use std::{
+    env::{self},
+    path::Path,
+    ptr,
+};
+
+#[derive(Debug)]
+pub struct Eric;
+
+impl Eric {
+    pub fn new(log_path: &Path) -> Result<Self, anyhow::Error> {
+        println!("Initializing eric");
+
+        let plugin_path =
+            env::var("PLUGIN_PATH").context("Missing environment variable 'PLUGIN_PATH'")?;
+        let plugin_path = Path::new(&plugin_path);
+
+        println!("Setting plugin path '{}'", plugin_path.display());
+        println!("Setting log path '{}'", log_path.display());
+        println!("Logging to '{}'", log_path.join("eric.log").display());
+
+        let plugin_path = plugin_path.try_to_cstring()?;
+        let log_path = log_path.try_to_cstring()?;
+
+        let error_code = unsafe { EricInitialisiere(plugin_path.as_ptr(), log_path.as_ptr()) };
+
+        match error_code {
+            x if x == ErrorCode::ERIC_OK as i32 => Ok(Eric),
+            error_code => Err(anyhow!("Can't init eric: {}", error_code)),
+        }
+    }
+
+    pub fn validate(
+        &self,
+        xml: String,
+        tax_type: &str,
+        tax_version: &str,
+        pdf_name: Option<&str>,
+    ) -> Result<EricResponse, anyhow::Error> {
+        let processing_flag: ProcessingFlag;
+        let type_version = format!("{}_{}", tax_type, tax_version);
+        let print_config = if let Some(pdf_name) = pdf_name {
+            processing_flag = ProcessingFlag::Print;
+            Some(PrintConfig::new(pdf_name, &processing_flag)?)
+        } else {
+            processing_flag = ProcessingFlag::Validate;
+            None
+        };
+        Self::process(xml, type_version, processing_flag, print_config, None, None)
+    }
+
+    pub fn send(
+        &self,
+        xml: String,
+        tax_type: &str,
+        tax_version: &str,
+        certificate_file: &str,
+        certificate_password: &str,
+        pdf_name: Option<&str>,
+    ) -> Result<EricResponse, anyhow::Error> {
+        let processing_flag: ProcessingFlag;
+        let type_version = format!("{}_{}", tax_type, tax_version);
+        let print_config = if let Some(pdf_name) = pdf_name {
+            processing_flag = ProcessingFlag::SendAndPrint;
+            Some(PrintConfig::new(pdf_name, &processing_flag)?)
+        } else {
+            processing_flag = ProcessingFlag::Send;
+            None
+        };
+        let certificate_config = CertificateConfig::new(certificate_file, certificate_password)?;
+        Self::process(
+            xml,
+            type_version,
+            processing_flag,
+            print_config,
+            Some(certificate_config),
+            None,
+        )
+    }
+
+    #[allow(dead_code)]
+    fn decrypt(
+        &self,
+        encrypted_file: &str,
+        certificate_config: CertificateConfig,
+    ) -> Result<i32, anyhow::Error> {
+        let encrypted_data = encrypted_file.try_to_cstring()?;
+        let response_buffer = ResponseBuffer::new()?;
+
+        let error_code = unsafe {
+            EricDekodiereDaten(
+                certificate_config.certificate.handle,
+                certificate_config.password.as_ptr(),
+                encrypted_data.as_ptr(),
+                response_buffer.as_ptr(),
+            )
+        };
+
+        Ok(error_code)
+    }
+
+    fn process(
+        xml: String,
+        type_version: String,
+        processing_flag: ProcessingFlag,
+        print_config: Option<PrintConfig>,
+        certificate_config: Option<CertificateConfig>,
+        transfer_code: Option<u32>,
+    ) -> Result<EricResponse, anyhow::Error> {
+        println!("Processing xml file");
+
+        match processing_flag {
+            ProcessingFlag::Validate => println!("Validating xml file"),
+            ProcessingFlag::Print => println!("Validating xml file"),
+            ProcessingFlag::Send => println!("Sending xml file"),
+            ProcessingFlag::SendAndPrint => println!("Send and print"),
+            ProcessingFlag::CheckHints => println!("Check hints"),
+        }
+
+        let xml = xml.try_to_cstring()?;
+        let type_version = type_version.try_to_cstring()?;
+
+        // Transfer_code should be NULL except for data retrieval; if
+        // transfer_code is not NULL in the other cases, it will be ignored
+        let transfer_code = match transfer_code {
+            Some(mut code) => &mut code,
+            None => ptr::null::<u32>() as *mut u32,
+        };
+
+        match &print_config {
+            Some(print_config) => println!(
+                "Printing confirmation to file '{}'",
+                print_config.pdf_name.to_str()?
+            ),
+            None => (),
+        }
+
+        let validation_response_buffer = ResponseBuffer::new()?;
+        let server_response_buffer = ResponseBuffer::new()?;
+
+        let error_code = unsafe {
+            EricBearbeiteVorgang(
+                xml.as_ptr(),
+                type_version.as_ptr(),
+                processing_flag.into_u32(),
+                // SAFETY: match a reference of print_config; otherwise
+                // print_config is moved, and print_parameter.as_ptr() would be
+                // dangling
+                match &print_config {
+                    Some(el) => el.print_parameter.as_ptr(),
+                    None => ptr::null(),
+                },
+                // SAFETY: match a reference of certificate_config; otherwise
+                // certificate_config is moved, and
+                // certificate_parameter.as_ptr() would be dangling
+                match &certificate_config {
+                    Some(el) => el.certificate_parameter.as_ptr(),
+                    None => ptr::null(),
+                },
+                transfer_code,
+                validation_response_buffer.as_ptr(),
+                server_response_buffer.as_ptr(),
+            )
+        };
+
+        // TODO: EricHoleFehlerText() for error code
+
+        let transfer_code = unsafe { transfer_code.as_ref() };
+
+        if let Some(code) = transfer_code {
+            println!("Transfer code: {}", code)
+        }
+
+        let validation_response = validation_response_buffer.read()?;
+        // TODO: parse server response via EricGetErrormessagesFromXMLAnswer()
+        let server_response = server_response_buffer.read()?;
+        let response = EricResponse::new(
+            error_code,
+            validation_response.to_string(),
+            server_response.to_string(),
+        );
+
+        Ok(response)
+    }
+}
+
+impl Drop for Eric {
+    fn drop(&mut self) {
+        println!("Closing eric");
+
+        // TODO: implement EricEntladePlugins
+
+        let error_code = unsafe { EricBeende() };
+
+        match error_code {
+            x if x == ErrorCode::ERIC_OK as i32 => (),
+            error_code => panic!("Can't close eric: {}", error_code),
+        }
+    }
+}
--- eric-sdk/src/lib.rs
+++ eric-sdk/src/lib.rs
@@ -1,1 +1,31 @@
+mod certificate;
+mod config;
+mod eric;
+mod error_code;
+mod response;
+mod utils;
 
+pub use eric::Eric;
+pub use error_code::ErrorCode;
+pub use response::EricResponse;
+
+#[derive(Debug, Clone, Copy)]
+pub(crate) enum ProcessingFlag {
+    Validate = 2,
+    Send = 4,
+    Print = 32,
+    SendAndPrint = 36,
+    #[allow(dead_code)]
+    CheckHints = 128,
+}
+
+impl ProcessingFlag {
+    pub fn into_u32(&self) -> u32 {
+        *self as u32
+    }
+}
+
+pub(crate) enum Preview {
+    Yes = 1,
+    No = 0,
+}
